546cc4267c99179e756170ef415e040ecdda40f1
Léo Hackin <leonardo.freire@giran.com.br>
2010-07-29 19:15:26 -0300

    initial push

---
+++ Tests/cadastroTest.php
@@ -0,0 +1,13 @@
+<?php
+require_once 'PHPUnit/Framework.php';
+require_once '../cadastro.class.php';
+ 
+class CadastroTest extends PHPUnit_Framework_TestCase
+{
+    public function test_deveria_retornar_o_que_enviei()
+    {
+        $cadastro = new Cadastro();
+        $this->assertEquals('Voce disse blz',$cadastro->say('blz'));
+    }
+}
+?>
---
+++ cadastro.php
@@ -0,0 +1,9 @@
+<?php
+
+require_once('cadastro.class.php');
+
+$cadastro = new Cadastro();
+
+$cadastro->say();
+
+?>
