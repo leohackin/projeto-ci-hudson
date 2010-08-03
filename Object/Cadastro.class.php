7ecf674ebfe9ea115021d8816ae28ae690a4888a
Léo Hackin <leonardo.freire@giran.com.br>
2010-08-03 18:29:50 -0300

    Oh my

---
+++ Object/Cadastro.class.php
@@ -0,0 +1,22 @@
+<?php
+
+	/**
+	 * Classe de teste de cadastro
+	 */
+	class Cadastro {
+		
+		public function __construct() {
+			
+		}
+		
+		/**
+     * função diz qualquer coisa
+     * @parameter $phrase 
+     */
+		public function say($phrase = 'Qualquer coisa') {
+			return "Voce disse " . $phrase;
+		}
+		
+	}
+
+?>
