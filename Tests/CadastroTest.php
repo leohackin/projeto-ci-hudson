9fcd68d738109794a21308e2db1a60dabf6694d7
Léo Hackin <leonardo.freire@giran.com.br>
2010-08-03 18:28:03 -0300

    teste

---
+++ Tests/CadastroTest.php
@@ -0,0 +1,13 @@
+<?php
+
+require_once 'Object/Cadastro.class.php';
+ 
+class CadastroTest extends PHPUnit_Framework_TestCase
+{
+    public function test_deveria_retornar_o_que_enviei()
+    {
+        $cadastro = new Cadastro();
+        $this->assertEquals('Voce disse blz',$cadastro->say('blz'));
+    }
+}
+?>
